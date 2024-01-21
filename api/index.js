ead29ab7226321646b84fb8bef8181d979efe050
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-01-21 11:21:34 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config()
 
+mongoose.connect(process.env.MANGO).then(()=>{
+    console.log('database is connected')
+}).catch((error)=>{
+    console.log(error.message)
+})
 const app=express();
 
 
