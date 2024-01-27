dcc4609273d7a9b1d2d04ae5722ed14e7cebf2b5
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-01-27 18:35:04 +0530

    add redux persist

--- blog-app/src/pages/SignIn.jsx
+++ blog-app/src/pages/SignIn.jsx
@@ -1,13 +1,20 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error: errorMessage} = useSelector((state) => state.user);
 
-  const navigate=useNavigate()
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,12 +22,11 @@ const SignIn = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if ( !formData.email || !formData.password) {
-      return setErrorMessage("Plase fill out all fields.");
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure("Plase fill out all fields."));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -28,15 +34,14 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+       dispatch(signInFailure(data.message));
       }
-      if(res.ok){
-        return navigate('/')
+      if (res.ok) {
+        dispatch(signInSuccess(data))
+        return navigate("/");
       }
-      setLoading(false);
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+  
     }
   };
 
