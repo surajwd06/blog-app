c4c19b6851b1f97fc29f8d8a4e543c4ce039c671
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-10 14:22:50 +0530

    complete the sidebar of the dashboard

---
+++ blog-app/src/components/DashSidebar.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <>
+      <Sidebar className="w-full">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Link to="/dashboard?tab=profile">
+              <Sidebar.Item
+                active={tab === "profile"}
+                icon={HiUser}
+                label={"user"}
+                labelColor="dark"
+              >
+                Profile
+              </Sidebar.Item>
+            </Link>
+            <Sidebar.Item
+              active
+              icon={HiArrowSmRight}
+              className="cursor-pointer"
+            >
+              Sign Out
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </>
+  );
+};
+
+export default DashSidebar;
--- blog-app/src/pages/Dashboard.jsx
+++ blog-app/src/pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
 
-export default Dashboard
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
+  return (
+    <>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="md:w-56">
+          <DashSidebar />
+        </div>
+        {tab === "profile" && <DashProfile />}
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
